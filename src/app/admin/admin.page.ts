b8cd67969496dfefdd998a8a65106643a0bca834
Priyanka Rohokale <Priyanka.Rohokale@nielsen.com>
2019-06-09 22:10:34 +0530

    changes

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -8,13 +8,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AdminPage implements OnInit {
   users = [];
+  usersCopy = [];
+  searchValue: string = "";
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get('./assets/users.json').subscribe((users: any) => {
       console.log("users list is -->", users);
       this.users = users;
+      this.usersCopy = JSON.parse(JSON.stringify(this.users));
     })
+  }
+
+  searchValues(event) {
+    console.log(this.searchValue);
+    let valueToMatch = this.searchValue.toLowerCase();
+
+    this.users = this.usersCopy.filter(user => {
+        return user.firstName.toLowerCase().includes(valueToMatch) || user.lastName.toLowerCase().includes(valueToMatch) || user.employeeId.toLowerCase().includes(valueToMatch)
+      
+    });
+    // category.data = technologies
+
   }
 
 }
